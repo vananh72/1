2a8b07bc46c284067f82554a72799d5887d774bd
Van Anh <kimvananh0702@gmail.com>
2020-10-25 22:23:27 +0700

    vd

---
+++ B10/main.js
@@ -0,0 +1,114 @@
+// const compareVs100 = number => {
+//     if (10 > 100) return true
+//     else return false
+//rut gon lai thanh:
+//     return number > 100
+// }
+// const getExamResult = score =>  score > 5 ? 'Pass' : 'Fail'
+
+// const findX = number => {
+//     if (number ===1 )return 1
+//     else if (number === 2)return 2
+//     else return 3
+// }
+
+// const findX2 = number => {
+//     const myNumber = 1
+//     switch (number) {
+//         case myNumber:
+//          {
+//             console.log('Toan')
+//             break
+//         }
+//         case 2: {
+//             console.log('Li')
+//         //    break
+//         }
+//         case 3: {
+//             console.log('Hoa')
+//         //    break
+//         }
+//         default: {
+//             console.log('Van')
+//             break
+//         }
+//     }
+// }
+
+// const myFunc = () => {
+//     let i= 1
+//     while (i < 11) {
+//         console.log(i)
+//         ++i
+//     }
+// }
+
+// const myFunc = () => {
+//     let i=100
+//     do {
+//         console.log('lop hoc js')
+//         console.log(i)
+//         ++i
+//     }while (i<11)
+// }
+
+// for (let i=1; i<11; i++){
+//     console.log(i)
+// }
+
+//const fruits = ['apple', 'orange', 'mango']
+
+// const myFruits ={
+//     0:apple
+// }
+const student = {
+    name:'Nguyen Van A',
+    age:'16'
+}
+
+// for(let key in student) {
+//     if(student.has0WnProperty(key)){
+//     console.log(student[key],key)
+//     }
+// }
+
+//for(let i=0;i<fruits.length ;i++){
+//    console.log(fruits[i])
+//    console.log(fruits[3])
+//}
+
+// const logFruit =fruit => {
+//     console.log(fruit)
+// }
+
+// fruits.forEach(logFruit)
+
+// fruits.forEach((fruit,index) => {
+//     console.log(fruit,index)
+// })
+
+// fruits.forEach(function(fruit) {
+//     console.log(fruit)
+// })
+
+// const max =7
+// for(let i=1;i<11; i++){
+//     console.log(i)
+//     if(i>=max) break;
+// }
+
+// const arr = [
+//     [1,2,3],
+//     [4,5,6],
+//     [7,8,9]
+// ]
+
+// arr.forEach(row => {
+//     console.log(row)
+
+//     row.forEach(col => {
+//         console.log(col)
+//     })
+//     console.log('------')
+// })
+// ar[2][2]
